0b7b0f5f1c2fc024c92e4a513a802458e2d2499d
Mark <mwemekamp@gmail.com>
2020-10-22 09:04:09 +0200

    Reverted IFacadeConfiguration Added nuspec file Gitignore clean up

--- RecaptchaV3/Forms/Recaptcha3.cs
+++ RecaptchaV3/Forms/Recaptcha3.cs
@@ -1,7 +1,6 @@
 ﻿using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Net;
 using System.Web;
 using Umbraco.Forms.Core;
@@ -12,22 +11,18 @@ using Umbraco.Forms.Core.Models;
 
 namespace Recaptcha.Forms
 {
-    public class Recaptcha3 : FieldType
+    public sealed class Recaptcha3 : FieldType
     {
-        private readonly IFacadeConfiguration _configuration;
-
-        public Recaptcha3(IFacadeConfiguration configuration)
+        public Recaptcha3()
         {
-            _configuration = configuration;
-
-            this.Id = new Guid("08b8057f-06c9-4ca5-8a42-fd1fc2a46eff");
-            this.Name = "Recaptcha3";
-            this.Description = "Render a Recaptcha v3.";
-            this.Icon = "icon-eye";
-            this.DataType = FieldDataType.String;
-            this.SortOrder = 10;
-            this.SupportsRegex = true;
-            this.HideLabel = true;
+            Id = new Guid("08b8057f-06c9-4ca5-8a42-fd1fc2a46eff");
+            Name = "Recaptcha3";
+            Description = "Render a Recaptcha v3.";
+            Icon = "icon-eye";
+            DataType = FieldDataType.String;
+            SortOrder = 10;
+            SupportsRegex = true;
+            HideLabel = true;
         }
 
         [Setting("Additional Settings", PreValues = "homepage,login,social,e-commerce", Description = "Use case", View = "Dropdownlist")]
@@ -37,7 +32,7 @@ namespace Recaptcha.Forms
         {
             var returnStrings = new List<string>();
             var token = HttpContext.Current.Request["g-recaptcha-response"];
-            var secret = _configuration.GetSetting("RecaptchaPrivateKey");
+            var secret = Configuration.GetSetting("RecaptchaPrivateKey");
             var client = new WebClient();
             var jsonResult = client.DownloadString(string.Format("https://www.google.com/recaptcha/api/siteverify?secret={0}&response={1}", secret, token));
             var obj = JsonConvert.DeserializeObject<CaptchaResponse>(jsonResult);
